223143179f88307d72b00d1252ad2835db72f228
Alexey Efimik <a.efimik@gmail.com>
2023-01-16 15:35:51 +0400

    123

--- Python/Lesson_10_ORM/homework_1/table_post.py
+++ Python/Lesson_10_ORM/homework_1/table_post.py
@@ -1,11 +1,16 @@
-from database import Base
+from database import Base, engine
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 import datetime
 
 class Post(Base):
-    __tablename__ = "posts"
+    __tablename__ = "post"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    body = Column(String)
-    published = Column(DateTime, default=datetime.datetime.utcnow)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    text = Column(String)
+    topic = Column(String)
+
+business_posts_ids = session.query(Post.id).finter(Post.topic == "business").order_by(Post.id.desc()).limit(10).all()
+
+print([id[0] for id in business_posts_ids])
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
