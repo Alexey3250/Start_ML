f805d420b81c85b09a5eeb9003577634e89569e1
Alexey Efimik <a.efimik@gmail.com>
2023-04-22 01:22:50 +0400

    uff

--- Final_project/pipe.py
+++ Final_project/pipe.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
+import catboost
 from catboost import Pool, CatBoostClassifier, CatBoost
 import numpy as np
 import re
@@ -79,25 +80,6 @@ def process_timestamps(data):
     
     return data
 
-# Кодирование категориальных признаков
-def encode_categorical_features(data):
-    # One-hot encoding для 'country', 'city' и 'topic'
-    data = pd.get_dummies(data, columns=['country', 'city', 'topic'], prefix=['country', 'city', 'topic'])
-
-    le_gender = LabelEncoder()
-    le_os = LabelEncoder()
-    le_source = LabelEncoder()
-    le_action = LabelEncoder()
-
-    # Label encoding для 'gender', 'os' и 'source'
-    data['gender'] = le_gender.fit_transform(data['gender'])
-    data['os'] = le_os.fit_transform(data['os'])
-    data['source'] = le_source.fit_transform(data['source'])
-    data['action'] = le_action.fit_transform(data['action'])
-
-    print('Categorical features encoded')
-    return data
-
 # Создание дополнительных признаков
 def create_additional_features(data):
     # Feature 1: Количество просмотров и лайков для каждого пользователя
@@ -111,193 +93,65 @@ def create_additional_features(data):
     data = data.merge(post_views_likes, on='post_id', how='left')
 
     # Feature 3: Количество просмотров и лайков для каждой группы тематик
-    temp_df = data[['exp_group', 'topic_business', 'topic_covid', 'topic_entertainment', 'topic_movie', 'topic_politics', 'topic_sport', 'topic_tech', 'action']]
-    for col in ['topic_business', 'topic_covid', 'topic_entertainment', 'topic_movie', 'topic_politics', 'topic_sport', 'topic_tech']:
-        temp_df.loc[:, col] = temp_df[col] * temp_df['action']
-    grouped_data = temp_df.groupby('exp_group').sum().reset_index()
-    grouped_data.columns = ['exp_group'] + [f'{col}_exp_group_views' if i % 2 == 0 else f'{col}_exp_group_likes' for i, col in enumerate(grouped_data.columns[1:], 1)]
+    temp_df = data[['exp_group', 'topic', 'action']]
+
+    # Создание колонок с количеством просмотров и лайков для каждой темы внутри группы
+    topic_action_count = temp_df.pivot_table(index='exp_group', columns=['topic', 'action'], aggfunc=len, fill_value=0)
+    topic_action_count.columns = [f'{col[0]}_exp_group_{col[1]}s' for col in topic_action_count.columns]
+    grouped_data = topic_action_count.reset_index()
+
     data = data.merge(grouped_data, on='exp_group', how='left')
+
+    # Преобразование категориальных признаков в строковый формат
+    categorical_columns = ['country', 'city', 'topic', 'gender', 'os', 'source']
+    data[categorical_columns] = data[categorical_columns].astype(str)
 
     print('Additional features created')
     return data
 
-# Обработка текстовых признаков
-def process_text_features(data):
-    def word_count(X):
-        return np.array([len(re.findall(r'\b\w+\b', text)) for text in X])
-    def sentence_count(X):
-        return np.array([len(re.findall(r'[.!?]+', text)) for text in X])
-    def avg_word_length(X):
-        return np.array([sum(len(word) for word in re.findall(r'\b\w+\b', text)) / len(re.findall(r'\b\w+\b', text)) if len(re.findall(r'\b\w+\b', text)) > 0 else 0 for text in X])
-    def punctuation_count(X):
-        return np.array([sum(1 for char in text if char in punctuation) for text in X])
+# Подготовка данных для инференса
+def prepare_data_for_prediction(data):
+    # Убираем ненужные столбцы
+    X = data.drop(['target', 'action', 'text'], axis=1)
 
-    # Применение функций извлечения признаков к столбцу 'text'
-    word_counts = word_count(data['text'])
-    sentence_counts = sentence_count(data['text'])
-    avg_word_lengths = avg_word_length(data['text'])
-    punctuation_counts = punctuation_count(data['text'])
+    categorical_columns = ['country', 'topic', 'city', 'gender', 'os', 'source']
 
-    # Добавление новых признаков в виде столбцов в DataFrame
-    data['word_count'] = word_counts
-    data['sentence_count'] = sentence_counts
-    data['avg_word_length'] = avg_word_lengths
-    data['punctuation_count'] = punctuation_counts
-
-    print('Text features processed')
-    return data
-
-# Обработка TF-IDF
-def process_tfidf(data):
-    # Инициализация TfidfVectorizer
-    vectorizer = TfidfVectorizer(max_features=1000) # Вы можете настроить max_features в зависимости от ваших потребностей
-    
-    # Обучение vectorizer на столбце 'text' и преобразование текстовых данных
-    tfidf_matrix = vectorizer.fit_transform(data['text'])
-
-    # Преобразование матрицы TF-IDF в DataFrame
-    tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer.get_feature_names_out())
-
-    # Конкатенация исходных данных с DataFrame TF-IDF
-    data_with_tfidf = pd.concat([data.drop(columns=['text']), tfidf_df], axis=1)
-
-    print('TF-IDF processed')
-    return data_with_tfidf
-
-
-'''
-ФУНККЦИИ ДЛЯ ОБУЧЕНИЯ МОДЕЛИ 
-'''
-# Отбор признаков на основе Mutual Information
-''' 
-# Отбор признаков на основе взаимной информации
-def select_features_mi(data, target_col, k=50):
-    X = data.drop([target_col], axis=1)
-    y = data[target_col]
-    
-    # Вычисление взаимной информации между каждым признаком и целевой переменной
-    mi_scores = mutual_info_classif(X, y, random_state=42)
-
-    # Создание DataFrame с именами признаков и соответствующими им оценками MI
-    mi_scores_df = pd.DataFrame({'feature': X.columns, 'mi_score': mi_scores})
-
-    # Сортировка DataFrame по оценкам MI в порядке убывания
-    mi_scores_df = mi_scores_df.sort_values('mi_score', ascending=False)
-
-    # При необходимости выберите k лучших признаков с помощью SelectKBest
-    selector = SelectKBest(mutual_info_classif, k=k)
-    selector.fit(X, y)
-    selected_features = X.columns[selector.get_support()]
-
-    print("Top k features based on mutual information:")
-    print(selected_features)
-
-    return selected_features
-'''
-
-# Функция тренировки модели CatBoost
-'''
-# Обучение модели CatBoost
-def train_catboost_model(X_train, X_test, y_train, y_test, group_id_col):
-    # Сортируем наборы данных для обучения и тестирования по 'group_id'
-    X_train = X_train.sort_values(by=group_id_col)
-    y_train = y_train.loc[X_train.index]
-    
-    X_test = X_test.sort_values(by=group_id_col)
-    y_test = y_test.loc[X_test.index]
-
-    # Создание объектов Pool для обучения и тестирования с указанием столбца 'group_id'
-    train_pool = Pool(X_train.drop(columns=[group_id_col]), y_train, group_id=X_train[group_id_col])
-    test_pool = Pool(X_test.drop(columns=[group_id_col]), y_test, group_id=X_test[group_id_col])
-
-    # Обучение модели CatBoost с использованием метрики оценки PrecisionAt:top=5
-    model = CatBoostClassifier(iterations=1000,
-                            learning_rate=0.1,
-                            depth=6,
-                            custom_metric='PrecisionAt:top=5',
-                            eval_metric='PrecisionAt:top=5',
-                            random_seed=42,
-                            verbose=100)
-
-    model.fit(train_pool, eval_set=test_pool)
-
-    return model
-
-# Сохранение и загрузка модели CatBoost
-def save_and_load_catboost_model(model, model_path):
-    model.save_model(model_path)
-    loaded_model = CatBoostClassifier()
-    loaded_model.load_model(model_path)
-
-    return loaded_model
-'''
-
-
-'''
-ОСНОВНАЯ ЧАСТЬ ПРОГРАММЫ
-'''
-
-# Создали функцию для обработки данных
-def process_inference_data(data, preselected_features):
-    '''
-    На выходе мы получим обработанные данные, которые можно будет передать в модель для получения прогнозов.
-    '''
-    
-    # Обработка данных
-    data = process_timestamps(data)
-    data = encode_categorical_features(data)
-    data = create_additional_features(data)
-    data = process_text_features(data)
-    data = process_tfidf(data)
-        
-    # Добавление отсутствующих колонок и заполнение их нулями
-    for col in preselected_features:
-        if col not in data.columns:
-            data[col] = 0
-
-    # Оставить только предварительно отобранные признаки
-    data = data[preselected_features]
-    
-    return data
-
-# Функция для получения прогнозов
-def catboost_inference(model, inference_data):
-    """
-    Функция выполняет предсказания для каждого пользователя в данных.
-
-    Аргументы:
-        model: обученная модель CatBoost.
-        inference_data: DataFrame с данными для предсказания.
-
-    Возвращает:
-        predictions: массив с предсказаниями.
-    """
-
-    # Создаем столбец 'group_id' на основе столбца 'user_id'
-    unique_user_ids = inference_data['user_id'].unique()
+    # Создание ID группы на основе столбца 'user_id'
+    unique_user_ids = X['user_id'].unique()
     group_id_dict = {user_id: idx for idx, user_id in enumerate(unique_user_ids)}
-    inference_data['group_id'] = inference_data['user_id'].map(group_id_dict)
+    X['group_id'] = X['user_id'].map(group_id_dict)
 
-    # Удаляем столбцы 'user_id' и 'group_id' перед выполнением предсказаний
-    inference_data_no_ids = inference_data.drop(columns=['user_id', 'group_id'])
+    # Сортировка набора данных для предсказаний по 'group_id'
+    X = X.sort_values(by='group_id')
 
-    # Выполняем предсказания
-    predictions = model.predict(inference_data_no_ids)
+    # Убедитесь, что категориальные переменные представлены в виде строк
+    X[categorical_columns] = X[categorical_columns].astype(str)
 
-    return predictions
+    # Получение индексов категориальных столбцов
+    cat_features = [X.drop(columns=['user_id']).columns.get_loc(col) for col in categorical_columns]
 
-# Функция для сохранения прогнозов
-def save_predictions(predictions, file_name):
-    # Ensure the "predictions" subfolder exists
-    if not os.path.exists("predictions"):
-        os.makedirs("predictions")
+    # Создание объекта Pool для набора данных предсказаний с колонкой 'group_id' и категориальными признаками
+    prediction_pool = Pool(X.drop(columns=['user_id']), cat_features=cat_features, group_id=X['group_id'])
 
-    # Save predictions to CSV file in the "predictions" subfolder
-    predictions_df = pd.DataFrame(predictions, columns=["prediction"])
-    predictions_df.to_csv(f"predictions/{file_name}", index=False)
+    return prediction_pool
 
-    print(f"Predictions saved to 'predictions/{file_name}'.")
+def process_and_save_predictions(predictions, inference_data, file_name):
+    predictions_df = pd.DataFrame(predictions, columns=['target'])
+    predictions_df['user_id'] = inference_data['user_id'].reset_index(drop=True)
+    predictions_df['post_id'] = inference_data['post_id'].reset_index(drop=True)
 
+    grouped = predictions_df.groupby('user_id')
+    top_5_predictions = grouped.apply(lambda x: x.nlargest(5, 'target')['post_id']).reset_index()
+
+    top_5_predictions['rank'] = top_5_predictions.groupby('user_id').cumcount() + 1
+
+    top_5_predictions_wide = top_5_predictions.pivot_table(index='user_id', columns='rank', values='post_id')
+    top_5_predictions_wide.columns = [f"Top_Prediction_{i}" for i in range(1, 6)]
+
+    top_5_predictions_wide.reset_index(inplace=True)
+    top_5_predictions_wide.to_csv(file_name, index=False)
+
+    print(f"Top-5 predictions saved to '{file_name}'.")
 
 '''
 ВАЖНЫЕ ПЕРЕМЕННЫЕ
@@ -308,30 +162,11 @@ engine = create_engine(
         "postgres.lab.karpov.courses:6432/startml"
     )
 
-# Отобранные признаки для модели
-preselected_features = ['user_id', 'gender', 'exp_group', 'os', 'day_of_week', 'hour_of_day',
-    'time_since_last_action', 'country_Russia', 'city_Kansk', 'city_Kazan',
-    'city_Mangghystaū', 'city_Michurinsk', 'city_Moscow', 'city_Pavlovo',
-    'city_Samara', 'topic_covid', 'topic_entertainment', 'topic_movie',
-    'user_views', 'user_likes', 'post_views',
-    'topic_business_exp_group_likes', 'topic_covid_exp_group_views',
-    'topic_entertainment_exp_group_likes', 'topic_movie_exp_group_views',
-    'topic_politics_exp_group_likes', 'topic_sport_exp_group_views',
-    'topic_tech_exp_group_likes', 'action_exp_group_views',
-    'sentence_count', 'punctuation_count', 'australian', 'based', 'city',
-    'ending', 'failed', 'given', 'idea', 'life', 'likely', 'opportunity',
-    'order', 'paid', 'party', 'past', 'political', 'robert', 'scene',
-    'seem', 'wanted']
-
 # Сколько рядов данных загружать за один раз
-feed_data_size = 1000
+feed_data_size = 100000
 
 data = pd.DataFrame()
 
-'''
-ГЛАВНАЯ ФУНКЦИЯ
-'''
-# Загрузка и обработка данных для инференса -> Обработка и сохранение результатов предсказаний
 def main():
     # Загрузка обученной модели
     model_path = "models/catboost_MAP_model.cbm"
@@ -339,20 +174,45 @@ def main():
     print("Model loaded successfully.")
     
     # Загружаем данные
-    data = load_and_merge_data(engine, feed_data_size)
+    inference_data = load_and_merge_data(engine, feed_data_size)
     print("Data loaded and merged successfully.")
     
     # Обработка данных
-    data = process_inference_data(data, preselected_features)
+    inference_data = process_timestamps(inference_data)
+    inference_data = create_additional_features(inference_data)
     print("Data processed successfully.")
-    print(f"Data shape after process_inference_data: {data.shape}")
+    print(f"Data shape after process_inference_data: {inference_data.shape}")
     
-    # Выполнение инференса
-    predictions = catboost_inference(model, data)
-    print("Predictions made successfully.")
+    # Shape data for prediction
+    prediction_pool = prepare_data_for_prediction(inference_data)
+    print("Data shaped for prediction successfully.")
+    
+    # Предсказание
+    predictions = model.predict(prediction_pool)
+    
+    
+    
+    # Сохранение результатов предсказаний в виде DataFrame
+    predictions_df = pd.DataFrame(predictions, columns=['target'])
+    predictions_df['user_id'] = inference_data['user_id'].reset_index(drop=True)
+    predictions_df['post_id'] = inference_data['post_id'].reset_index(drop=True)
 
-    # Save predictions to a CSV file
-    save_predictions(predictions, "predictions.csv")
+    # Группировка по user_id и сортировка по 'target' в порядке убывания
+    grouped = predictions_df.groupby('user_id')
+    top_5_predictions = grouped.apply(lambda x: x.nlargest(5, 'target')['post_id']).reset_index()
+
+    # Группировка данных
+    top_5_predictions['rank'] = top_5_predictions.groupby('user_id').cumcount() + 1
+
+    # Преобразование данных в широкий формат
+    top_5_predictions_wide = top_5_predictions.pivot_table(index='user_id', columns='rank', values='post_id')
+    top_5_predictions_wide.columns = [f"Top_Prediction_{i}" for i in range(1, 6)]
+
+    # Сброс индекса и сохранение предсказаний в CSV-файл
+    top_5_predictions_wide.reset_index(inplace=True)
+    top_5_predictions_wide.to_csv("top_5_predictions_wide.csv", index=False)
+
+    print("Top-5 predictions saved to 'top_5_predictions_wide.csv'.")
 
 if __name__ == "__main__":
     main()
