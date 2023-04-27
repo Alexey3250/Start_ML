713034505538b7ab577398284e1899794d5be1ce
Alexey Efimik <a.efimik@gmail.com>
2023-04-28 00:55:15 +0400

    123

--- Final_project/fast_api/service.py
+++ Final_project/fast_api/service.py
@@ -1,14 +1,90 @@
+# Импортируем необходимые модули и библиотеки
 import os
+from catboost import CatBoostClassifier
+import pandas as pd
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from schema import PostGet
 from datetime import datetime
 
+# Определяем функцию для получения пути к модели
+def get_model_path(path: str) -> str:
+    # Проверяем, выполняется ли код в LMS, и устанавливаем путь для загрузки модели
+    if os.environ.get("IS_LMS") == "1":
+        MODEL_PATH = '/workdir/user_input/model'
+    else:
+        MODEL_PATH = path
+    return MODEL_PATH
+
+# Определяем функцию для загрузки модели машинного обучения
+def load_models():
+    model_path = get_model_path("/my/super/path")
+    # Загружаем модель из файла по пути model_path
+    from_file = CatBoostClassifier()
+    model = from_file.load_model(model_path)
+    return model
+
+# Определяем функцию для получения данных из базы данных PostgreSQL
+def batch_load_sql(query: str) -> pd.DataFrame:
+    CHUNKSIZE = 200000
+    # Создаем подключение к базе данных
+    engine = create_engine(
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+    )
+    conn = engine.connect().execution_options(stream_results=True)
+    chunks = []
+    # Получаем данные из базы данных порциями, чтобы избежать переполнения памяти
+    for chunk_dataframe in pd.read_sql(query, conn, chunksize=CHUNKSIZE):
+        chunks.append(chunk_dataframe)
+    conn.close()
+    # Объединяем полученные порции данных в один датафрейм и возвращаем его
+    return pd.concat(chunks, ignore_index=True)
+
+# Определяем функцию для загрузки данных о признаках статей и пользователей
+def load_features() -> pd.DataFrame:
+    return batch_load_sql('SELECT * FROM public.yancharskaya_features_lesson_22')
+
+# Определяем переменные для подключения к базе данных
+SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Определяем класс Post для работы с таблицей базы данных post
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    topic = Column(String)
+
+# Определяем функцию для получения сессии базы данных
+def get_db():
+    with SessionLocal() as db:
+        return db
+
+# Определяем приложение FastAPI
 app = FastAPI()
 
+# Определяем функцию для получения списка рекомендуемых статей
 @app.get("/post/recommendations/", response_model=List[PostGet])
 def recommended_posts(
-		id: int, 
-		time: datetime, 
-		limit: int = 10) -> List[PostGet]:
-     pass
+		id: int,
+		time: datetime,
+		limit: int = 5,
+		db: Session = Depends(get_db)) -> List[PostGet]:
+	# Получаем признаки пользователя из таблицы признаков features
+	user_features = features[features['user_id']==id]
+	# Предсказываем вероятности понравившихся статей с помощью загруженной модели
+	user_features['proba'] = model.predict_proba(user_features)[:, 1]
+	# Выбираем limit статей с наибольшими вероятностями
+	preds_id = user_features.sort_values(by='proba', ascending=False)['post_id'][:limit].values.tolist()
+	preds = []
+	# Для каждой выбранной статьи выполняем запрос к базе данных, чтобы получить ее текст и тему
+	for i in preds_id:
+		preds.append(db.query(Post).filter(Post.id == i).one_or_none())
+	# Возвращаем список объектов PostGet, содержащий текст и тему выбранных статей
+	return preds
---
+++ Final_project/srvice/tricky_trick.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import time
+
+
+
+
+def load_features():
+    
+    engine = create_engine(
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+        )
+    
+    def batch_load_sql_timed(engine, query: str, chunksize: int) -> pd.DataFrame:
+        conn = engine.connect().execution_options(stream_results=True)
+        chunks = []
+        row_count = 0
+        start_time = time.time()
+
+        for chunk_dataframe in pd.read_sql(query, conn, chunksize=chunksize):
+            chunks.append(chunk_dataframe)
+            row_count += len(chunk_dataframe)
+            print(f"Loaded {row_count} rows, elapsed time: {time.time() - start_time:.2f} seconds")
+
+        conn.close()
+        return pd.concat(chunks, ignore_index=True)
+    
+
+
+    chunksize = 1000000
+    
+    # Чтение данных таблицы user_data
+    query = "SELECT * FROM user_data"
+    user_data = pd.read_sql(query, engine)
+    
+    # Чтение ограниченного данных таблицы feed_data
+    query = "SELECT * FROM feed_data"
+    feed_data = batch_load_sql_timed(engine, query, chunksize)
+    
+    # Переименование столбцов идентификаторов
+    user_data = user_data.rename(columns={'id': 'user_id'})
+
+    # Объединение таблиц
+    data = feed_data.merge(user_data, on='user_id', how='left')
+    
+    return data
+
+def main():
+        
+    engine = create_engine(
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+    )
+    data = load_features()
+    
+    return data
+
+if __name__ == "__main__":
+    main()
